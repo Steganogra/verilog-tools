1f6a7c9791138c53b34288730c46f036c57f0f29
Steganogra <steganogra@gmail.com>
2014-08-19 23:32:33 +0100

    Add macro test case

---
+++ vpp+/examples/macro.v
@@ -0,0 +1,11 @@
+// Example:
+//   python vpp+.py macro.vpp
+
+reg reg0;
+reg reg1;
+reg reg2;
+reg reg3;
+reg reg4;
+reg reg5;
+
+wire [3:0] test_wire;
--- vpp+/examples/regress.sh
+++ vpp+/examples/regress.sh
@@ -1,9 +1,9 @@
 python ../vpp+.py adder.vpp WIDTH=8 > adder.new
 python ../vpp+.py mux.vpp PORTS=4 WIDTH=8 > mux.new
 python ../vpp+.py recurse.vpp > recurse.new
+python ../vpp+.py macro.vpp > macro.new
 
-
-for TEST in adder mux recurse
+for TEST in adder mux recurse macro
 do
     diff -w $TEST.v $TEST.new
     rc=$?
